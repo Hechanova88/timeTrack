dcf08e522cb1b2c6bcfbd03034518e6841857b5a
Hechanova88 <79033769+Hechanova88@users.noreply.github.com>
2021-03-16 16:20:08 -0400

    added js

---
+++ function.js
@@ -0,0 +1,8 @@
+$ t switch World Domination Plan
+$ t in --at "5 minutes ago" Research Volcano Islands
+$ t out
+$ t display
+Timesheet World Domination Plan:
+Day            Start      End        Duration   Notes
+Mar 14, 2009   19:53:30 - 20:06:15   0:12:45    Research Volcano Islands
+Total                                0:12:45
