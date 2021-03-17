ad636bb1abd522a3807b5795ac57aae1694fef3d
Hechanova88 <79033769+Hechanova88@users.noreply.github.com>
2021-03-16 22:21:30 -0400

    time and date

--- function.js
+++ function.js
@@ -21,6 +21,11 @@ function fireEvent(action, message, options) {
     });
     return true;
   };
+  var currentdate = new Date();
+var datetime = "Last Sync: " + currentdate.getDay() + "/" + currentdate.getMonth() 
++ "/" + currentdate.getFullYear() + " @ " 
++ currentdate.getHours() + ":" 
++ currentdate.getMinutes() + ":" + currentdate.getSeconds();
   
   // example event on the page
   fireEvent("ajaxError", "XY page failed to load");
