bcae1ae3bb69ead0eb9c6d0671baa54447744a73
Hechanova88 <79033769+Hechanova88@users.noreply.github.com>
2021-03-30 23:59:00 -0400

    firebase database

--- function.js
+++ function.js
@@ -1,31 +1,16 @@
-// send a request about an event to the server
-function fireEvent(action, message, options) {
-    var loggerUrl = "/tracker.gif", parameters;
-    options = options || {};
-    options.url = options.url || window.location.href;
-    options.user_agent = navigator.userAgent;
-    options.message = message;
-    for (var i in options) {
-      if (options.hasOwnProperty(i)) {
-        parameters += "&" + i + "=" + encodeURIComponent(options[i]);
-      }
-    }    
-    new Image().src = loggerUrl + parameters;
-  }
-  
-  // log script errors
-  window.onerror = function(errorMessage, url, line) {   
-    fireEvent("error", errorMessage, {
-      url: url, 
-      line: line
-    });
-    return true;
-  };
-  var currentdate = new Date();
-var datetime = "Last Sync: " + currentdate.getDay() + "/" + currentdate.getMonth() 
-+ "/" + currentdate.getFullYear() + " @ " 
-+ currentdate.getHours() + ":" 
-+ currentdate.getMinutes() + ":" + currentdate.getSeconds();
-  
-  // example event on the page
-  fireEvent("ajaxError", "XY page failed to load");
+const userId = document.getElementById('userId');
+const userEmail = document.getElementById('userEmail');
+const userMesage = document.getElementById('userMessage');
+const addBtn = document.getElementById('addBtn');
+const updateBtn = document.getElementById('updateBtn'); 
+const removeBtn = document.getElementById('removeBtn'); 
+
+const database = firebase.database();
+
+addBtn.addEventListener('click',(e) => {
+  e.preventDefault();
+  database.ref('/users/'+ userId.value).set({
+    user_id: userEmail.value,
+    user_message: userMessage.value,
+  })
+});
