59d567cdec07e5e09a94867ef78fac342eb012c0
Hechanova88 <79033769+Hechanova88@users.noreply.github.com>
2021-03-31 17:00:58 -0400

    message out update

--- function.js
+++ function.js
@@ -1,16 +1,22 @@
 const userId = document.getElementById('userId');
-const userEmail = document.getElementById('userEmail');
-const userMesage = document.getElementById('userMessage');
+const firstName = document.getElementById('firstName');
+const lastName = document.getElementById('lastName');
+const age = document.getElementById('age');
 const addBtn = document.getElementById('addBtn');
-const updateBtn = document.getElementById('updateBtn'); 
-const removeBtn = document.getElementById('removeBtn'); 
+const updateBtn = document.getElementById('updateBtn');
+const removeBtn = document.getElementById('removeBtn');
+const message = document.getElementById('message');
+
 
 const database = firebase.database();
 
-addBtn.addEventListener('click',(e) => {
-  e.preventDefault();
-  database.ref('/users/'+ userId.value).set({
-    user_id: userEmail.value,
-    user_message: userMessage.value,
-  })
-});
+addBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    database.ref('/user/' + userId.value).set({
+        first_name: firstName.value,
+        last_name: lastName.value,
+        age: age.value,
+        message: message.value,
+    })
+    
+})
